f2e4049c619fa4e55b47ba7ed7f6b28b4802609a
digiborimusik <digiborimusik@gmail.com>
2023-08-31 12:27:43 +0300

    extract dataset

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,33 @@
-import { Person } from "./entity/person";
 import { PersonSearchServiceESImpl } from "./services/person.search.service";
+import { personsDataSet } from "./utils/datasets";
 
-(async function init() {
+(async function main() {
   try {
-    console.log("Hello Worlds");
-
     const search = new PersonSearchServiceESImpl({ url: "http://127.0.0.1:9200" });
 
+    // Wipe and recreate index
     await search.deleteIndex();
     await search.createIndex();
+    await search.indexPersons(personsDataSet);
 
-    const persons: Person[] = [
-      {
-        id: "uid1",
-        firstName: "John",
-        lastName: "Doe",
-        interests: ["hiking", "biking", "swimming"],
-        age: 32,
-        job: "Software Engineer",
-        location: {
-          lat: 47.377,
-          lon: 8.539,
-        },
-      },
-      {
-        id: "uid2",
-        firstName: "Jane",
-        lastName: "Doe",
-        interests: ["hiking", "biking", "singing"],
-        age: 30,
-        job: "Teacher",
-        location: {
-          lat: 33.37,
-          lon: 33.53,
-        },
-      },
-    ];
-
-    await search.indexPersons(persons);
-
-    // Wait for data to be indexed
+    // Wait until index is ready
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    const hikingPeoplesNearby = await search.matchInterestedPersonsNearby({
-      interests: ["swimming"],
+    // Search for persons with interests
+    const interestPeoplesNearby = await search.matchInterestedPersonsNearby({
+      // interests: ["hiking"],
+      // interests: ["swimming"],
+      interests: ["smoking", "drinking"],
       location: {
         lat: 47.377,
         lon: 8.539,
       },
-      distanceKm: 10000,
+      // distanceKm: 1000
+      // distanceKm: 10000,
+      distanceKm: 100000,
     });
 
-    console.log(hikingPeoplesNearby);
+    console.log(interestPeoplesNearby);
   } catch (error) {
     console.error(error);
     process.exit(1);
---
+++ src/utils/datasets.ts
@@ -0,0 +1,112 @@
+import { Person } from "../entity/person";
+
+export const personsDataSet: Person[] = [
+  {
+    id: "uid1",
+    firstName: "John",
+    lastName: "Doe",
+    interests: ["hiking", "biking", "swimming"],
+    age: 32,
+    job: "Software Engineer",
+    location: {
+      lat: 47.377,
+      lon: 8.539,
+    },
+  },
+  {
+    id: "uid2",
+    firstName: "Jane",
+    lastName: "Doe",
+    interests: ["hiking", "biking", "singing"],
+    age: 30,
+    job: "Teacher",
+    location: {
+      lat: 33.37,
+      lon: 33.53,
+    },
+  },
+  {
+    id: "uid3",
+    firstName: "Max",
+    lastName: "Mustermann",
+    interests: ["swimming", "biking", "singing"],
+    age: 25,
+    job: "Student",
+    location: {
+      lat: 49.228,
+      lon: 7.928,
+    },
+  },
+  {
+    id: "uid3",
+    firstName: "Artur",
+    lastName: "Chicar",
+    interests: ["drinking", "smoking", "chattering"],
+    age: 30,
+    job: "Security",
+    location: {
+      lat: 51.726,
+      lon: 22.646,
+    },
+  },
+  {
+    id: "uid4",
+    firstName: "Johanna",
+    lastName: "Peterson",
+    interests: ["reading", "writing", "running"],
+    age: 43,
+    job: "Writer",
+    location: {
+      lat: 44.556,
+      lon: 37.321,
+    },
+  },
+  {
+    id: "uid5",
+    firstName: "Marta",
+    lastName: "Kowalski",
+    interests: ["drinking", "smoking", "running"],
+    age: 23,
+    job: "Security",
+    location: {
+      lat: 51.726,
+      lon: 22.646,
+    },
+  },
+  {
+    id: "uid6",
+    firstName: "Ricardo",
+    lastName: "Perez",
+    interests: ["running", "dancing", "beatboxing"],
+    age: 14,
+    job: "Schoolboy",
+    location: {
+      lat: 55.726,
+      lon: 21.646,
+    },
+  },
+  {
+    id: "uid7",
+    firstName: "Lil",
+    lastName: "Poop",
+    interests: ["drinking", "smoking", "beatboxing"],
+    age: 25,
+    job: "Gangster",
+    location: {
+      lat: 57.432,
+      lon: 21.226,
+    },
+  },
+  {
+    id: "uid8",
+    firstName: "Lisa",
+    lastName: "Simpson",
+    interests: ["dancing", "hiking", "swimming"],
+    age: 19,
+    job: "Trainee",
+    location: {
+      lat: 47.347,
+      lon: 18.539,
+    },
+  },
+];
